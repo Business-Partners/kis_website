52715b045d2f95dfe8c4c257d1ebc913de1f6f71
trupakufi <marioteixeiravarela@gmail.com>
2023-05-31 15:37:53 +0100

    ci: Add Docker Compose

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import sassDts from "vite-plugin-sass-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sassDts()],
+  server: {
+    host: true,
+    port: 8000,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
